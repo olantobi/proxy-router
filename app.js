cf89b264b5cce2858ffc7fa366bcb12dac5d721a
Tobi Olanrewaju <olantobi@gmail.com>
2020-07-13 15:53:51 +0100

    Removed other attempts

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 const http = require('http');
 const https = require('https');
-const net = require('net');
-const { URL } = require('url');
 
-// Create an HTTP tunneling proxy
-const proxy = http.createServer((req, res) => {
-    const { headers, method, url } = req;
+function onRequest(request, response) {
+    const options = {
+        hostname: process.env.HOST_URL,
+        port: process.env.HOST_PORT,
+        path: request.url,
+        headers: request.headers,
+        method: request.method,
+    };
 
-    console.log("Host: ", process.env.HOST_URL);
-    console.log("PORT: ", process.env.HOST_PORT);
+    const proxy = (process.env.PROTOCOL === 'http')
+        ? http.request(options, function (res) {
+        response.writeHead(res.statusCode, res.headers);
+        res.pipe(response, {
+            end: true
+        });
 
-    let body = [];
-    req.on('error', (err) => {
-        console.error(err);
-    }).on('data', (chunk) => {
-        body.push(chunk);
-    }).on('end', () => {
-        body = Buffer.concat(body).toString();
+        console.log(request.method,  request.url, res.statusCode);
+    }) :
+        https.request(options, function (res) {
+            response.writeHead(res.statusCode, res.headers);
+            res.pipe(response, {
+                end: true
+            });
 
-        const options = {
-            host: process.env.HOST_URL,
-            port: process.env.HOST_PORT,
-            path: url,
-            method: method,
-            headers: headers,
-            body: body
-        }
-
-        http.request(options, (response) => {
-            response.pipe(res, { end: true });
+            console.log(request.method,  request.url, res.statusCode);
         })
+
+    request.pipe(proxy, {
+        end: true
     });
-}).listen(9090);
+}
+
+http.createServer(onRequest).listen(9090);
